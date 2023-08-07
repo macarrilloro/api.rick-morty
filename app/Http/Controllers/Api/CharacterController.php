98cbb3d0f8cb6d95769c68fc9d824605c3ba8aa7
Miguel Angel CARRILLO ROA <miguel-angel.carrillo-roa@bureauveritas.com>
2023-08-07 14:00:53 -0500

    Actualización del middleware auth api

--- app/Http/Controllers/Api/CharacterController.php
+++ app/Http/Controllers/Api/CharacterController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class CharacterController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except(['index','show']);
+        // $this->middleware('auth:api')->only(['store','update','destroy']);
+    }
     /**
      * Display a listing of the resource.
      */
